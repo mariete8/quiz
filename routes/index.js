cd7a7a086d2e0fe3d7709bf832c327d910f0dc05
mariete8 <mariete_aragon@hotmail.com>
2016-06-06 17:10:29 +0200

    P12

--- routes/index.js
+++ routes/index.js
@@ -13,6 +13,12 @@ router.get('/question',quizController.question);
 router.get('/check',quizController.check);
 router.get('/quizzes.:format?', middlewares);
  router.get('/quizzes/:quizId(\\d+).:format?', middlewares);
-
+router.get('/users', userController.index);   // listado usuarios
+router.get('/users/:userId(\\d+)',      userController.show);    // ver un usuario
+router.get('/users/new',                userController.new);     // formulario sign un
+router.post('/users',                   userController.create);     // registrar usuario
+router.get('/users/:userId(\\d+)/edit', sessionController.loginRequired, sessionController.adminOrMyselfRequired, userController.edit);    // editar cuenta
+router.get('/users/:userId(\\d+)', sessionController.loginRequired, sessionController.adminOrMyselfRequired, userController.update);    // actualizar cuenta
+router.delete('/users/:userId(\\d+)', sessionController.loginRequired, sessionController.adminAndNotMyselfRequired, userController.destroy);    // borrar cuenta
 
 module.exports = router;
