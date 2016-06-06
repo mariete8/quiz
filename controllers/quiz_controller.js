cbe8079b29fc970f728812bf68b7cdc727dfa73c
mariete8 <mariete_aragon@hotmail.com>
2016-06-06 17:05:59 +0200

    P10

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -11,3 +11,15 @@ var answer=req.query.answer||"";
 
 	res.render('quizzes/result', { result: result,answer:answer  });
 };
+//GET/index
+exports.index = function(req, res, next) {
+  if(req.query.search){
+    var texto = '%' + req.query.search.toString()+'%';
+    texto = texto.replace(/ /g, "%"); //replace espacios por %
+    models.Quiz.findAll({where: ["question like ?", texto], include: [models.Attachment]})
+        .then(function(quizzes) {
+            res.render('quizzes/index',
+                {
+                    quizzes: quizzes
+                });
+    }).catch(function(error){next(error);});
